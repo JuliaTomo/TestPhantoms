ccd036d70821b103496113f0ef050e1456eafa88
jakeoung <jakeoung@gmail.com>
2020-06-16 16:26:05 +0200

    minor

---
+++ src/test_spheres.jl
@@ -0,0 +1,36 @@
+include("../src/analytic/spheres.jl")
+
+nangles = 30
+angles = 0:pi/nangles:pi
+angles = collect(angles)[1:end-1]
+height=64
+width=64
+
+centers = Array([10.0 5.0 0]')
+radii = [10.0]
+p = gen_spheres(centers, radii, angles, height, width)
+
+using Plots
+ani = @animate for ang=1:length(angles)
+    plot(Gray.(p[ang,:,:] ./ maximum(p[ang,:,:])), title="ang: $(angles[ang])")
+end
+
+p ./= height
+
+gif(ani, "~/Desktop/test_spheres.gif", fps=4)
+
+# save result
+using NPZ, BSON
+using BSON: @save
+ddata = "../data/sphere/"
+mkpath(ddata)
+fproj = "$ddata/sinogram.npy"
+NPZ.npzwrite(fproj, p)
+
+proj_geom = Dict(:type=>"parallel3d",:ProjectionAngles=>angles,
+                :DetectorRowCount=>height, :DetectorColCount=>width,
+                :DetectorSpacingX=>2/height, :DetectorSpacingY=>2/width)
+
+@save "$ddata/proj_geom.bson" proj_geom
+# @save "$ddata/proj_geom.bson" proj_geom
+# @save fproj p
